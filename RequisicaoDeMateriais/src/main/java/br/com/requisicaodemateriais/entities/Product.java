dffcdf5a7eb95fc6fb2fd0c87fce9636ed088012
Cleisson Heggdorne <cleissonrosa@hotmail.com>
2023-04-04 07:49:04 -0300

    impl: Validação de controle de acesso do usuário na al_ctrl_acesso. fix: Correção da classe User.

--- RequisicaoDeMateriais/src/main/java/br/com/requisicaodemateriais/entities/Product.java
+++ RequisicaoDeMateriais/src/main/java/br/com/requisicaodemateriais/entities/Product.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
--- RequisicaoDeMateriais/src/main/java/br/com/requisicaodemateriais/entities/User.java
+++ RequisicaoDeMateriais/src/main/java/br/com/requisicaodemateriais/entities/User.java
@@ -11,7 +11,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
